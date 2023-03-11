cb9bdadec0a882ac050fb5bb00b4848cc3679ff7
FreeVeganRecipes <127599289+FreeVeganRecipes@users.noreply.github.com>
2023-03-11 14:17:02 +0100

    Update recipes.js

--- recipes.js
+++ recipes.js
@@ -1,98 +1,66 @@
-// get all recipe links
+// Get all recipe links on the page
 const recipeLinks = document.querySelectorAll('.recipe-link');
 
-// loop through each link and add a click event listener
-recipeLinks.forEach((link) => {
-  link.addEventListener('click', function(event) {
-    // prevent the link from navigating to a new page
-    event.preventDefault();
-
-    // get the filename of the recipe
-    const href = this.getAttribute('href');
-    const filename = href.split('/').pop().replace('.html', '.md');
-
-    // fetch the markdown content for the recipe
-    fetch(`../_recipes/${filename}`)
-      .then(response => response.text())
-      .then(data => {
-        // parse the YAML front matter
-        const [frontMatter, content] = data.split('---\n').slice(1);
-        const recipeData = yaml.load(frontMatter);
-
-        // parse the recipe content as HTML
-        const converter = new showdown.Converter();
-        const html = converter.makeHtml(content);
-
-        // create a new modal with the recipe data and content
-        const modal = createModal(recipeData, html);
-        document.body.appendChild(modal);
-
-        // add event listeners for the modal
-        const closeButton = modal.querySelector('.modal-close');
-        closeButton.addEventListener('click', function() {
-          modal.remove();
-        });
-
-        const adjustServings = modal.querySelector('.adjust-servings');
-        adjustServings.addEventListener('input', function() {
-          const servingCount = parseInt(this.value);
-
-          if (isNaN(servingCount)) {
-            return;
-          }
-
-          const recipeIngredients = modal.querySelectorAll('.recipe-ingredient');
-          recipeIngredients.forEach((ingredient) => {
-            const originalAmount = ingredient.getAttribute('data-amount');
-            const scaledAmount = originalAmount / recipeData.servings * servingCount;
-            ingredient.querySelector('.ingredient-amount').textContent = scaledAmount.toFixed(1);
-          });
-        });
-      });
+// Loop through each recipe link and add a click event listener
+recipeLinks.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    // Get the filename of the recipe markdown file
+    const filename = link.getAttribute('href').replace('.html', '.md');
+    // Create a modal element for the recipe
+    createModal(filename);
   });
 });
 
-// function to create a modal element with the recipe data and content
-function createModal(recipeData, recipeContent) {
-  const modal = document.createElement('div');
-  modal.classList.add('modal');
+// Function to create a modal element for a recipe
+function createModal(filename) {
+  // Fetch the recipe data and content
+  fetch(`./_recipes/${filename}`)
+    .then(response => response.text())
+    .then(recipeData => {
+      // Split the recipe data into YAML front matter and content
+      const [yamlData, recipeContent] = recipeData.split('---\n').slice(1);
+      // Parse the YAML front matter
+      const recipe = yaml.load(yamlData);
+      // Parse the recipe content as HTML
+      const converter = new showdown.Converter();
+      const recipeHTML = converter.makeHtml(recipeContent);
+      // Create a new modal element with the recipe data and content
+      const modal = document.createElement('div');
+      modal.classList.add('modal');
+      modal.innerHTML = `
+        <div class="modal-content">
+          <span class="close-modal">&times;</span>
+          <h2>${recipe.title}</h2>
+          <p><strong>Creator:</strong> ${recipe.creator}</p>
+          <p><strong>Date:</strong> ${recipe.date}</p>
+          <div class="recipe-content">${recipeHTML}</div>
+          <div class="recipe-servings">
+            <label for="servings">Servings:</label>
+            <input type="number" id="servings" name="servings" value="${recipe.servings}" min="1">
+          </div>
+        </div>
+      `;
+      // Add the modal to the DOM
+      document.body.appendChild(modal);
+      // Add event listeners to the modal
+      const closeModal = modal.querySelector('.close-modal');
+      closeModal.addEventListener('click', () => modal.remove());
+      const adjustServings = modal.querySelector('#servings');
+      adjustServings.addEventListener('input', () => adjustIngredientAmounts(recipeHTML, recipe.servings, adjustServings.value));
+    });
+}
 
-  const modalOverlay = document.createElement('div');
-  modalOverlay.classList.add('modal-overlay');
-  modal.appendChild(modalOverlay);
-
-  const modalContent = document.createElement('div');
-  modalContent.classList.add('modal-content');
-
-  const closeButton = document.createElement('button');
-  closeButton.classList.add('modal-close');
-  closeButton.textContent = '×';
-  modalContent.appendChild(closeButton);
-
-  const title = document.createElement('h2');
-  title.textContent = recipeData.title;
-  modalContent.appendChild(title);
-
-  const creator = document.createElement('p');
-  creator.textContent = `Created by ${recipeData.creator} on ${recipeData.date}`;
-  modalContent.appendChild(creator);
-
-  const servings = document.createElement('p');
-  servings.textContent = `Servings: ${recipeData.servings}`;
-  modalContent.appendChild(servings);
-
-  const servingsAdjuster = document.createElement('div');
-  servingsAdjuster.classList.add('servings-adjuster');
-  const servingsAdjusterLabel = document.createElement('label');
-  servingsAdjusterLabel.textContent = 'Adjust Servings: ';
-  const servingsAdjusterInput = document.createElement('input');
-  servingsAdjusterInput.classList.add('adjust-servings');
-  servingsAdjusterInput.setAttribute('type', 'number');
-  servingsAdjusterInput.setAttribute('min', 1);
-  servingsAdjusterInput.setAttribute('value', recipeData.servings);
-  servingsAdjuster.appendChild(servingsAdjusterLabel);
-  servingsAdjuster.appendChild(servingsAdjusterInput);
-  modalContent.appendChild(servingsAdjuster);
-
-  const content = document.createElement('div');
-  content.innerHTML
+// Function to adjust the ingredient amounts based on the number of servings
+function adjustIngredientAmounts(recipeHTML, oldServings, newServings) {
+  const recipeContent = document.querySelector('.recipe-content');
+  // Get all ingredient amounts in the recipe
+  const ingredientAmounts = recipeHTML.match(/\d+\s*(\/\s*\d+)?\s*(cup|cups|tbsp|tsp|ounces|pounds|g|kg)/gi);
+  // Loop through each ingredient amount and adjust it based on the new number of servings
+  ingredientAmounts.forEach(amount => {
+    const oldAmount = amount.split(' ')[0];
+    const unit = amount.split(' ')[1];
+    const newAmount = parseFloat(((oldAmount / oldServings) * newServings).toFixed(2));
+    recipeContent.innerHTML = recipeContent.innerHTML.replace(amount, `${newAmount} ${unit}`);
+  });
+}
