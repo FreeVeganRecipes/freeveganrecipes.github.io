1009686231d242122fe46b3f9f3c4c0c43e96680
FreeVeganRecipes <127599289+FreeVeganRecipes@users.noreply.github.com>
2023-03-11 14:08:26 +0100

    Update recipes.js

--- recipes.js
+++ recipes.js
@@ -1,27 +1,39 @@
-// Define the path to the _recipes directory
-var recipesPath = "_recipes/";
+// get all recipe links on the page
+const recipeLinks = document.querySelectorAll('.recipe-link');
 
-// Fetch the list of recipe files
-fetch(recipesPath)
-  .then(response => response.text())
-  .then(data => {
-    // Split the data into an array of filenames
-    var filenames = data.trim().split("\n");
+// loop through each recipe link and fetch its markdown content
+recipeLinks.forEach(link => {
+  fetch(link.href)
+    .then(response => response.text())
+    .then(text => {
+      // convert the markdown to HTML and insert it into the page
+      const converter = new showdown.Converter();
+      const html = converter.makeHtml(text);
+      link.insertAdjacentHTML('afterend', html);
 
-    // Loop through each filename and generate a link to the recipe
-    var recipeList = document.getElementById("recipes");
-    for (var i = 0; i < filenames.length; i++) {
-      var filename = filenames[i].trim();
+      // add event listener to quantity input for each recipe
+      const quantityInput = document.querySelector(`#${link.dataset.recipe}-quantity-input`);
+      if (quantityInput) {
+        quantityInput.addEventListener('input', () => {
+          // get the ingredient list and loop through each ingredient
+          const ingredientList = document.querySelector(`#${link.dataset.recipe}-ingredients-list`);
+          const ingredients = ingredientList.querySelectorAll('li');
+          ingredients.forEach(ingredient => {
+            // get the quantity and unit from the ingredient
+            const ingredientText = ingredient.textContent.trim();
+            const quantityMatch = ingredientText.match(/^\d+\/?\d*|^\d*\.\d+|^\d+/);
+            const quantity = quantityMatch ? parseFloat(quantityMatch[0]) : null;
+            const unitMatch = ingredientText.match(/(?<=\d\s*)\w+|\w+$/);
+            const unit = unitMatch ? unitMatch[0] : '';
 
-      // Generate a link to the recipe using the filename
-      var link = document.createElement("a");
-      link.href = recipesPath + filename;
-      link.textContent = filename.replace(".md", "");
+            // calculate the new quantity based on the input value
+            const newQuantity = quantityInput.value * quantity;
 
-      // Add the link to the recipe list
-      var listItem = document.createElement("li");
-      listItem.appendChild(link);
-      recipeList.appendChild(listItem);
-    }
-  })
-  .catch(error => console.error(error));
+            // update the ingredient text with the new quantity and unit
+            const newIngredientText = `${newQuantity.toFixed(1)} ${unit} ${ingredientText.substring(quantityMatch[0].length + unit.length + 1)}`;
+            ingredient.textContent = newIngredientText;
+          });
+        });
+      }
+    });
+});
